70de87a50e81f4b271e1df45d3054f0f9720e803
quantum.v.rl <quantum.v.rl@gmail.com>
2020-02-06 00:52:03 -0800

    git commit fixed errors

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,4 +1,4 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
