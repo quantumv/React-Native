1df8d1905460929da9b04673136078c1ec2b1420
quantum.v.rl <quantum.v.rl@gmail.com>
2020-02-09 11:22:47 -0800

    git commit AboutComponent.js

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
 import { PARTNERS } from '../shared/partners';
+
+function RenderMission({mission}) {
+    if (mission) {
+        return (
+            <Card
+            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            The community partnership in
+            >
+            </Card>
+        );
+    }
+}
 
 class About extends Component {
     constructor(props) {
@@ -15,8 +28,27 @@ class About extends Component {
     };
 
     render() {
+        const { navigate } = this.props.navigation;
+        const renderPartnerItem = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={'./images/bootstrap-logo.png'}
+                />
+            );
+        };
         return(
-            <ScrollView />
+            <ScrollView>
+                <RenderMission />
+                <Card title="Community Partners">
+                    <FlatList 
+                        data={this.state.mission}
+                        renderItem={renderPartnerItem}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </Card>
+            </ScrollView>
         );
     }
 }
